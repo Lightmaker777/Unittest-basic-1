715cf63985cbe9265c960ca04c3817303ca20bb4
Lightmaker777 <j.kochkorian@gmx.de>
2023-09-22 14:06:00 +0200

    added solutions

--- src/text.py
+++ src/text.py
@@ -1,11 +1,11 @@
-# function will convert string parameter to upper case
-def to_upper(str):
-    return str.upper()
+# src/text.py
 
-# function will check return true if all items on
-# the parameter list are upper case
-def to_word_list_isupper(str_list):
-    for word in str_list:
-        if word.islower():
-            return False
-    return True
+def to_upper(text):
+    if not isinstance(text, str):
+        raise TypeError("Input must be a string")
+    return text.upper()
+
+def to_word_list_isupper(words):
+    if not isinstance(words, list):
+        raise TypeError("Input must be a list")
+    return all(word.isupper() for word in words)
